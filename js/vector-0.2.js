e4738936ef10795f57afe4f0d09868be3f34d951
stephband <stephband@googlemail.com>
2016-01-13 16:09:26 +0100

    Moves properties to prototype. JSON Stringifies to array.

--- js/vector-0.2.js
+++ js/vector-0.2.js
@@ -1,34 +1,31 @@
 // vector.js
-// 0.3
 //
 // Provides a Vector constructor (that works with or without the 'new'
 // keyword) which takes a pair of cartesian values as an array, object
 // or a pair of arguments, or a pair of polar values as an {a, d} object.
-//
+// 
 // Properties:
-//
+// 
 // x, y - cartesian coordinates
 // a, d - polar coordinates
-//
+// 
 // Methods:
-//
+// 
 // add(vector)        - returns a new vector
 // subtract(vector)   - returns a new vector
 // equals(vector)     - tests one vector against another
-// toCartesian()      - returns cartesian coordinates as an array [x, y]
-// toPolar()          - returns polar coordinates as an array [distance, angle]
-// toString()         -
+// toCartesianArray()
+// toPolarArray()
+// toString()
 
 
 (function(ns, undefined){
 	"use strict";
-
-	var assign = Object.assign;
-
-	function setPolar(obj) {
+	
+	function addPolar(obj) {
 		var x = obj.x,
 		    y = obj.y;
-
+		
 		obj.a = Math.atan2(x, y);
 		obj.d = x === 0 ?
 				Math.abs(y) :
@@ -36,73 +33,56 @@
 				Math.abs(x) :
 				Math.sqrt(x*x + y*y) ;
 	};
-
-	function setCartesian(obj) {
-		var d = obj.d;
-		var a = obj.a;
-
+	
+	function addCartesian(obj) {
+		var d = obj.d,
+		    a = obj.a;
+		
 		obj.x = Math.sin(a) * d;
 		obj.y = Math.cos(a) * d;
 	};
-
-	function clearCartesian(obj) {
-		obj.x = undefined;
-		obj.y = undefined;
+	
+	function deleteCartesian(obj) {
+		delete obj.x;
+		delete obj.y;
 	}
-
-	function clearPolar(obj) {
-		obj.a = undefined;
-		obj.d = undefined;
+	
+	function deletePolar(obj) {
+		delete obj.a;
+		delete obj.d;
+	}
+	
+	function makeProperty(x, y, data, set, get) {
+		return {
+			set: function(n) {
+				if (data[y] === undefined) { get(data); }
+				set(data);
+				data[x] = n;
+			},
+			
+			get: function() {
+				if (data[x] !== undefined) { return data[x]; }
+				get(data);
+				return data[x];
+			},
+			
+			enumerable: true,
+			configurable: false
+		};
 	}
 
 	function throwError(str) {
 		throw new Error(str);
 	}
-
-	function makeVectorProperty(p1, p2, p3, p4, set, clear) {
-		return {
-			get: function() {
-				var data = this.data;
-
-				if (typeof data[p1] !== "number" &&
-				    typeof data[p3] === "number" &&
-				    typeof data[p4] === "number") {
-					set(data);
-				}
-
-				return data[p1];
-			},
-
-			set: function(n) {
-				var data = this.data;
-
-				if (data[p1] === n) { return; }
-
-				if (typeof data[p2] !== "number" &&
-				    typeof data[p3] === "number" &&
-				    typeof data[p4] === "number") {
-					set(data);
-				}
-
-				if (typeof data[p2] === "number") {
-					clear(data);
-				}
-
-				data[p1] = n;
-			},
-
-			enumerable: true
-		};
-	}
-
+	
 	function Vector(arg0, arg1) {
 		var data;
-
+		
 		// Don't require the 'new' keyword
 		if (!(this instanceof Vector)) {
 			return new Vector(arg0, arg1);
 		}
-
+		
 		// Accept input arguments in any old form:
 		//
 		// x, y
@@ -111,9 +91,9 @@
 		// { left: x, top: y }
 		// [ x, y ]
 		// { width: x, height: y }
-		//
+		// 
 		// Passing no arguments will create the vector { X: 0, y: 0 }
-
+		
 		data = typeof arg0 === 'number' && typeof arg1 === 'number' ?
 			{ x: arg0, y: arg1 } :
 			arg0 === undefined && arg1 === undefined ?
@@ -131,79 +111,64 @@
 				{ x: arg0.width, y: arg0.height } :
 				throwError('Object passed to Vector() has no vector properties') :
 			throwError('Invalid arguments passed to Vector()') ;
-
+		
 		Object.defineProperties(this, {
-			data: { value: data }
+			x: makeProperty('x', 'y', data, deletePolar, addCartesian),
+			y: makeProperty('y', 'x', data, deletePolar, addCartesian),
+			a: makeProperty('a', 'd', data, deleteCartesian, addPolar),
+			d: makeProperty('d', 'a', data, deleteCartesian, addPolar)
 		});
 	}
-
-	var xProp = makeVectorProperty('x', 'y', 'a', 'd', setCartesian, clearPolar);
-	var yProp = makeVectorProperty('y', 'x', 'a', 'd', setCartesian, clearPolar);
-
-	Object.defineProperties(Vector.prototype, {
-		0: xProp,
-		1: yProp,
-		x: xProp,
-		y: yProp,
-		a: makeVectorProperty('a', 'd', 'x', 'y', setPolar, clearCartesian),
-		d: makeVectorProperty('d', 'a', 'x', 'y', setPolar, clearCartesian),
-		length: { value: 2 }
-	});
-
-	assign(Vector.prototype, {
-		// Encourage vector to display as an array in Web Inspector
-		splice: function(){},
-
+	
+	Vector.prototype = {
 		add: function(vector) {
 			if (vector === undefined) {
-				throw new Error('Vector: .add() called with undefined');
+				throw new Error('Vector#add() called with undefined argument');
 			}
-
+			
 			if (!(vector instanceof Vector)) {
 				vector = new Vector(arguments[0], arguments[1]);
 			}
-
-			return new Vector(this[0] + vector[0], this[1] + vector[1]);
+			
+			return new Vector(this.x + vector.x, this.y + vector.y);
 		},
-
+		
 		subtract: function(vector) {
 			if (vector === undefined) {
-				throw new Error('Vector: .subtract() called with undefined');
+				throw new Error('Vector#subtract() called with undefined argument');
 			}
-
+			
 			if (!(vector instanceof Vector)) {
 				vector = new Vector(arguments[0], arguments[1]);
 			}
-
-			return new Vector(this[0] - vector[0], this[1] - vector[1]);
+			
+			return new Vector(this.x - vector.x, this.y - vector.y);
 		},
-
+		
 		equals: function(vector) {
-			if (vector === undefined) { return false; }
-
+			if (vector === undefined) {
+				throw new Error('Vector#subtract() called with undefined argument');
+			}
+			
 			if (!(vector instanceof Vector)) {
 				vector = new Vector(arguments[0], arguments[1]);
 			}
-
-			return this[0] === vector[0] && this[1] === vector[1];
+			
+			return this.x === vector.x && this.y === vector.y;
 		},
-
-		toCartesian: function() {
+		
+		toString: function() {
+			return this.toCartesianArray.join(', ');
+		},
+		
+		toCartesianArray: function() {
 			return [this.x, this.y];
 		},
-
-		toPolar: function() {
+		
+		toPolarArray: function() {
 			return [this.d, this.a];
-		},
-
-		toJSON: function() {
-			return this.toCartesian();
-		},
-
-		toString: function() {
-			return this.toCartesian() + '';
 		}
-	});
-
+	};
+	
 	ns.Vector = Vector;
 })(window);
